d2f0f8b1e011a636824499d994e959975108df7b
egorb <egorbusalaev@gmail.com>
2023-05-26 17:04:57 +0700

    AOP для проекта
    добавил dependency B pom файл aspectj
    добавил конфигурации в application context
    
    добавил аспект корорый будет работать с методами репозитория ( DAO)

---
+++ src/main/java/com/egor/spring/mvc_hibernate/aspect/MyLoggingAspect.java
@@ -0,0 +1,26 @@
+package com.egor.spring.mvc_hibernate.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+    @Around("execution(* com.egor.spring.mvc_hibernate.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        String methodName = methodSignature.getName();
+
+        System.out.println("Начал работу " + methodName);
+
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        System.out.println("Закончил работу " + methodName);
+
+        return targetMethodResult;
+    }
+
+}
